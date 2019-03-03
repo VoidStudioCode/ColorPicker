301312a4e1e27b81c587e93edc76f92578c398a3
i605 <28020309+i605@users.noreply.github.com>
2019-03-03 10:16:12 +0800

    Add files via upload

---
+++ ColorPicker/ColorPicker/ColorPicker.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace ColorPicker
+{
+    public class ColorPicker
+    {
+        private Color GetColor(String path)
+        {
+            Bitmap bitmap = new Bitmap(path);
+            Int32 width = 20;
+            Int32 height = width * bitmap.Height / bitmap.Width;
+            Bitmap newBitmap = new Bitmap(bitmap, width, height);
+            bitmap.Dispose();
+
+            Rectangle rect = new Rectangle(0, 0, width, height);
+            BitmapData bmpData = newBitmap.LockBits(rect, ImageLockMode.ReadWrite, newBitmap.PixelFormat);
+            IntPtr ptr = bmpData.Scan0;
+            Int32 bytes = Math.Abs(bmpData.Stride) * height;
+            Byte[] sourcePixels = new Byte[bytes];
+            Marshal.Copy(ptr, sourcePixels, 0, bytes);
+            newBitmap.UnlockBits(bmpData);
+
+            List<Color> pixels = new List<Color>();
+            Int32 a, r, g, b;
+            Int32 count = 0;
+            Color color;
+
+            for (Int32 n = 0; n < sourcePixels.Length / 4; n++)
+            {
+                b = sourcePixels[count++];
+                g = sourcePixels[count++];
+                r = sourcePixels[count++];
+                a = sourcePixels[count++];
+                color = Color.FromArgb(r, g, b);
+                if (color.GetBrightness() < 0.8 && color.GetBrightness() > 0.3)
+                {
+                    pixels.Add(color);
+                }
+            }
+
+            color = Color.FromArgb(0, 0, 0);
+            count = 0;
+            foreach (Color selectedColor in pixels)
+            {
+                Int32 c = pixels.Count(comparedColor => Math.Abs(comparedColor.GetHue() - selectedColor.GetHue()) < 5 && Math.Abs(comparedColor.GetSaturation() - selectedColor.GetSaturation()) < 0.2 && selectedColor.GetSaturation() > 0.2);
+                if (c > count)
+                {
+                    count = c;
+                    color = selectedColor;
+                }
+            }
+            count = 0;
+            if (color.GetBrightness() == 0)
+            {
+                foreach (Color selectedColor in pixels)
+                {
+                    Int32 c = pixels.Count(comparedColor => Math.Abs(comparedColor.GetHue() - selectedColor.GetHue()) < 5);
+                    if (c > count)
+                    {
+                        count = c;
+                        color = selectedColor;
+                    }
+                }
+            }
+
+            return color;
+        }
+
+        public System.Windows.Media.Color GetMediaColor(String path)
+        {
+            Color color = GetColor(path);
+            return System.Windows.Media.Color.FromArgb(color.A, color.R, color.G, color.B);
+        }
+    }
+}
---
+++ ColorPicker/ColorPickerDemo/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace ColorPickerDemo
+{
+    public partial class MainWindow : Window
+    {
+        private readonly Stopwatch stopwatch = new Stopwatch();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_PreviewDragOver(Object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effects = DragDropEffects.Link;
+                Mouse.SetCursor(Cursors.Arrow);
+            }
+            else
+            {
+                e.Effects = DragDropEffects.None;
+            }
+            e.Handled = true;
+        }
+
+        private void Window_PreviewDrop(Object sender, DragEventArgs e)
+        {
+            Mouse.SetCursor(Cursors.AppStarting);
+            String path = ((Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();
+            OpenFile(path);
+        }
+
+        private void Button_Click(Object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog
+            {
+                Multiselect = false,
+                DefaultExt = ".jpg",
+                Filter = "Image File|*.jpg|*.png|*.bmp"
+            };
+            if (ofd.ShowDialog() == true)
+            {
+                OpenFile(ofd.FileName);
+            }
+        }
+
+        private void OpenFile(String path)
+        {
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.DecodePixelWidth = 300;
+            bitmap.DecodePixelHeight = 300;
+            bitmap.UriSource = new Uri(path, UriKind.Absolute);
+            bitmap.EndInit();
+            bitmap.Freeze();
+            Img.Source = bitmap;
+
+            stopwatch.Restart();
+            ColorPicker.ColorPicker colorPicker = new ColorPicker.ColorPicker();
+            Color color = colorPicker.GetMediaColor(path);
+            stopwatch.Stop();
+
+            Rec.Fill = new SolidColorBrush(color);
+            Tb_Time.Text = $"{stopwatch.ElapsedMilliseconds} ms";
+        }
+    }
+}
